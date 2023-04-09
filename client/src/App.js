f7770d6305a05acb5eb8874b4d7c0c574fed128a
sharif Habiman <hdidiersharif@gmail.com>
2023-04-09 20:37:23 +0200

    implimenting creating user

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,44 @@ import react,{useEffect,useState} from 'react';
 
 function App() {
  const [sectors,setSectors]=useState([])
+ const [name,setName]=useState("")
+ const[sector,setSector]=useState("")
+ const [agreeTerms,setagreeTerms]=useState("")
+ const [id,setUserId]=useState(undefined)
   useEffect(() => {
     fetch('https://coding-challenge-api.vercel.app/sectors').then(responce => responce.json()).then(data=>setSectors(data))
   }, []);
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+
+      const body = {name,sector:sector.replace(/^&nbsp;/, ''),agree_terms:agreeTerms,id };
+       fetch("https://coding-challenge-api.vercel.app/user", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      }).then(response => response.json())
+      .then(data => {
+        setUserId(data.user_id)
+        console.log(data)
+      })
+      .catch(error => console.error(error));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <div className="App">
        Please enter your name and pick the Sectors you are currently involved in.
       <br/>
       <br/>
       Name: 
-      <input type="text"/>
+      <input  value={name} onChange={(e)=>setName(e.target.value)} type="text"/>
       <br/>
       <br/>
       Sectors: 
-      <select multiple="" size="5">
+      <select  size="5" value={sector} onChange={(e)=>setSector(e.target.value)}>
         {sectors && sectors.map((sector)=>{
           return(
             <>
@@ -39,10 +63,10 @@ function App() {
       </select>
       <br/>
       <br/>
-      <input type="checkbox"/> Agree to terms
+      <input type="checkbox" onChange={(e)=>setagreeTerms(e.target.checked)}/> Agree to terms
       <br/>
       <br/>
-      <input type="submit" value="Save"></input>
+      <input type="submit" value="Save" onClick={onSubmitForm}></input>
     </div>
   );
 }
